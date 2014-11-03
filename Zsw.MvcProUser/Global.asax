e2bf4ce6dadcb8e5b3248690e6285448b25ec025
zhusongwen <k_qing@163.com>
2014-11-03 14:45:39 +0800

    study

---
+++ Zsw.MvcProUser/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Zsw.MvcProUser.MvcApplication" Language="C#" %>
---
+++ Zsw.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zsw.Common.Util;
+
+namespace Zsw.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadExcel readExcel = new ReadExcel();
+            readExcel.ConvertExcelToDataTable(@"D:\1.xlsx", true);
+
+
+
+            Console.Read();
+        }
+    }
+}
